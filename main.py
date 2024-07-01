0e4a9bc5fbfea4978ad08d3c3e3029e34e464281
Amanda-Lexine Sunga <152565370+amandalexine@users.noreply.github.com>
2024-07-01 17:15:21 +0000

    Added more One Direction lyrics

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from pyscript import document
+import random
 
 def sampleFunc(event) :
-  print("You've got that one thing!")
+    lyrics = ["You've got that one thing", "That's what makes you beautiful", 
+              "Nobody can drag me down", "If you ever feel alone, don't", 
+              "I wanna live while we're young", "Everything you do is magic",
+              "Nobody compares to you"]
+
+    print(random.choice(lyrics))
 
 thisButton = document.querySelector("#press")
 thisButton.onclick = sampleFunc
