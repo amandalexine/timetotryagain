8b27b518d7b291eb6b0eeeaf4ea348968378c005
Amanda-Lexine Sunga <152565370+amandalexine@users.noreply.github.com>
2024-07-01 16:04:49 +0000

    Added some logic that connects to the new lyric button

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello, world!")
+from pyscript import document
+
+def sampleFunc(event) :
+  print("You've got that one thing!")
+
+thisButton = document.querySelector("#press")
+thisButton.onclick = sampleFunc
