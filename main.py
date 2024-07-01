0a2a5ee2a560a7a72cca3e91d8e44dfc71b2fa69
Amanda Sunga <amandalexine025@gmail.com>
2024-07-01 13:28:46 -0400

    Added another One Direction lyric

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def sampleFunc(event) :
     lyrics = ["You've got that one thing", "That's what makes you beautiful", 
               "Nobody can drag me down", "If you ever feel alone, don't", 
               "I wanna live while we're young", "Everything you do is magic",
-              "Nobody compares to you"]
+              "Nobody compares to you", "You're insecure"]
 
     print(random.choice(lyrics))
 
