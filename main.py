54c9c9d9672696638c83ee075ea916eecb3e1b0a
Amanda Sunga <amandalexine025@gmail.com>
2024-07-01 15:10:25 -0400

    Added even more One Direction lyrics

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def sampleFunc(event) :
     lyrics = ["You've got that one thing", "That's what makes you beautiful", 
               "Nobody can drag me down", "If you ever feel alone, don't", 
               "I wanna live while we're young", "Everything you do is magic",
-              "Nobody compares to you", "You're insecure"]
+              "Nobody compares to you", "You're insecure",
+              "You and me got a whole lot of history",
+              "I can make your tears fall down like the showers that are British",
+              "The story of my life", "Even when the night changes, it'll never change me and you",
+              "It's gotta be you", "Reality ruined my life",
+              "We're like 'Na na na,' then we're like 'Yeah yeah yeah'",
+              "Baby I'm perfect for you", "You make me strong",
+              "I speak a different language, but I still hear you call"]
 
     print(random.choice(lyrics))
 
